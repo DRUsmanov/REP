abf02589bd5da2b21e8211f9ae53257e31c74b96
DRUsmanov <vangold0312@gmail.com>
2024-12-01 23:12:30 +0500

    Add new files

---
+++ LeetCode_Polindrome/LeetCode_Polindrome.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string word{ "полпаплоп" };
+    int i = (word.size() / 2);
+    for (int s = 0; s < i; s++) {
+        if (word[s] != word[word.size() - s - 1]) {
+            cout << "This is not palindrome!";
+            break;
+        }
+        if (s == i - 1) {
+            cout << "This is palindrome!";
+        }
+    }
+ return 0;
+}
---
+++ Remove Element/Remove Element.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Object{
+public:
+    vector<int> nums;
+    int val;
+    void RemoveElement() {
+        vector<int>::iterator it;
+        for (it=nums.begin(); it<nums.end();it++) {
+            if (*it == val) {
+                --it;
+                nums.erase(it+1);
+            }
+        }
+        for (it = nums.begin(); it < nums.end(); it++){
+            cout << *it << " ";
+        }
+    }
+    
+};
+
+int main()
+{
+    vector<int> nums={ 5,4,3,2,1,2,6,8,9,2,4,6 };
+    
+    Object Solution;
+    Solution.nums = nums;
+    Solution.val = 2;
+    Solution.RemoveElement();
+    
+    return 0;
+}
+
