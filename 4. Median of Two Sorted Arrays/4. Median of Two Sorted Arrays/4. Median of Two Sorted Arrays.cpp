302193c37088e72fa42da0c23d9d3b66695b93cd
DRUsmanov <vangold0312@gmail.com>
2025-01-02 00:25:18 +0500

    LC: 4. Median of Two Sorted Arrays

---
+++ 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main()
+{
+    vector<int> nums1{ 1,2 };
+    vector<int> nums2{ 4,3 };
+    double a;
+    nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+    sort(nums1.begin(), nums1.end());
+    nums1.size()  % 2 == 0 ? a=(double)(nums1[(nums1.size()/ 2) - 1] + nums1[nums1.size() / 2]) / 2   :a=(double)nums1[nums1.size() / 2];
+    cout << a;
+    return 0;
+}
+
+
+
